742042f9266f13c3de1b5d3bb97b7917ba8d6583
Subutai Ahmad <sahmad@numenta.com>
2014-05-13 15:52:30 -0700

    Initial commit - helper class for running anomaly likelihood computation

--- run_anomaly/run_anomaly.py
+++ run_anomaly/run_anomaly.py
@@ -27,6 +27,7 @@ A simple client to run CLA anomaly detection using the OPF.
 from optparse import OptionParser
 import sys
 import csv
+import math
 import datetime
 import dateutil.parser
 
@@ -35,8 +36,74 @@ from nupic.frameworks.opf.predictionmetricsmanager import MetricsManager
 
 import model_params
 
+import anomaly_likelihood
+
 def createModel():
   return ModelFactory.create(model_params.MODEL_PARAMS)
+
+
+class AnomalyLikelihood(object):
+  """
+  Helper class for running anomaly likelihood computation.
+  """
+  
+  def __init__(self, probationaryPeriod = 600, CLALearningPeriod = 300):
+    """
+    CLALearningPeriod - the number of iterations required for the CLA to
+    learn some of the patterns in the dataset.
+    
+    probationaryPeriod - no anomaly scores are reported for this many
+    iterations.  This should be CLALearningPeriod + some number of records
+    for getting a decent likelihood estimation.
+    
+    """
+    self._iteration          = 0
+    self._historicalScores   = []
+    self._distribution       = None
+    self._probationaryPeriod = probationaryPeriod
+    self._CLALearningPeriod  = CLALearningPeriod
+
+
+  def _computeLogLikelihood(self, likelihood):
+    """
+    Compute a log scale representation of the likelihood value. Since the
+    likelihood computations return low probabilities that often go into 4 9's or
+    5 9's, a log value is more useful for visualization, thresholding, etc.
+    """
+    # The log formula is:
+    # Math.log(1.0000000001 - likelihood) / Math.log(1.0 - 0.9999999999);
+    return math.log(1.0000000001 - likelihood) / -23.02585084720009
+
+
+  def likelihood(self, value, anomalyScore, dttm):
+    """
+    Given the current metric value, plus the current anomaly
+    score, output the anomalyLikelihood for this record.
+    """
+    dataPoint = (dttm, value, anomalyScore)
+    # We ignore the first probationaryPeriod data points
+    if len(self._historicalScores) < self._probationaryPeriod:
+      likelihood = 0.5
+    else:
+      # On a rolling basis we re-estimate the distribution every 100 iterations
+      if self._distribution is None or (self._iteration % 100 == 0): 
+        _, _, self._distribution = (
+          anomaly_likelihood.estimateAnomalyLikelihoods(
+            self._historicalScores,
+            skipRecords = self._CLALearningPeriod)
+          )
+        
+      likelihoods, _, self._distribution = (
+        anomaly_likelihood.updateAnomalyLikelihoods([dataPoint],
+          self._distribution)
+      )
+      likelihood = 1.0 - likelihoods[0]
+      
+    # Before we exit update historical scores and iteration
+    self._historicalScores.append(dataPoint)
+    self._iteration += 1
+
+    return likelihood
 
 
 def runAnomaly(options):
@@ -57,8 +124,12 @@ def runAnomaly(options):
     # Open file and setup headers
     reader = csv.reader(fin)
     csvWriter = csv.writer(open(options.outputFile,"wb"))
-    csvWriter.writerow(["timestamp", "value", "anomaly_score"])
+    csvWriter.writerow(["timestamp", "value",
+                        "anomaly_score", "likelihood_score"])
     headers = reader.next()
+    
+    # The anomaly likelihood object
+    anomalyLikelihood = AnomalyLikelihood()
     
     # Iterate through each record in the CSV file
     print "Starting processing at",datetime.datetime.now()
@@ -69,14 +140,20 @@ def runAnomaly(options):
       inputData["value"] = float(inputData["value"])
       inputData["dttm"] = dateutil.parser.parse(inputData["dttm"])
       
-      # Send it to the CLA and get back the results
+      # Send it to the CLA and get back the raw anomaly score
       result = model.run(inputData)
-      
-      # Retrieve the anomaly score and write it to a file
       anomalyScore = result.inferences['anomalyScore']
-      csvWriter.writerow([inputData["dttm"], inputData["value"],
-                          anomalyScore])
       
+      # Compute the Anomaly Likelihood
+      likelihood = anomalyLikelihood.likelihood(inputData["value"],
+                                                anomalyScore,
+                                                inputData["dttm"]
+                                                )
+
+      # Write results to the CSV file
+      csvWriter.writerow([inputData["dttm"], inputData["value"],
+                          anomalyScore, likelihood])
+
       # Progress report
       if (i%500) == 0: print i,"records processed"
 
