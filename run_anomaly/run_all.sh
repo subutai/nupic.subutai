003efc7222b4f99a93514a9d120eb2b8cb42d218
Subutai Ahmad <sahmad@numenta.com>
2014-06-09 16:55:38 -0700

    Added new data files to run_all

--- run_anomaly/run_all.sh
+++ run_anomaly/run_all.sh
@@ -3,3 +3,5 @@ python run_anomaly.py --inputFile data/rds_connections.csv --outputFile anomaly_
 python run_anomaly.py --inputFile data/cpu_cc0c5.csv --outputFile anomaly_scores_cpu_cc0c5.csv
 python run_anomaly.py --inputFile data/cpu_825cc.csv --outputFile anomaly_scores_cpu_825cc.csv
 python run_anomaly.py --inputFile data/cpu_5f553.csv --outputFile anomaly_scores_cpu_5f553.csv
+python run_anomaly.py --inputFile data/machine_temperature.csv --outputFile anomaly_scores_machine_temperature.csv --min 0 --max 110
+python run_anomaly.py --inputFile data/ambient_temperature.csv --outputFile anomaly_scores_ambient_temperature.csv --min 50 --max 100
